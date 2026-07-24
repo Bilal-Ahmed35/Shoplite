49b15f7c791e18cdbd2cd3a458cbc4c23bde3a18
Bilal-Ahmed35 <bilalahmedshah35@gmail.com>
2026-07-24 16:39:10 +0500

    adding useAuth() in Navbar  page

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
+    const { logout } = useAuth();
+    const router = useRouter();
+
+    const handleLogout = () => {
+        logout();
+        router.push("/login");
+    };
+
     return (
         <nav className="flex justify-center gap-6 bg-gray-900 p-4 text-white">
             <Link href="/">Landing</Link>
@@ -12,6 +24,13 @@ export default function Navbar() {
             <Link href="/login">Login</Link>
 
             <Link href="/about">About</Link>
+
+            <button
+                onClick={handleLogout}
+                className="bg-red-600 px-3 py-1 rounded"
+            >
+                Logout
+            </button>
         </nav>
     );
 }
